b3a9641935c11b54cdcff798cf7fc1be25b23ee6
Johnathan X. Liu <jxliu95@gmail.com>
2019-12-08 01:54:16 -0500

    Day 2 part 2 complete

--- day2.js
+++ day2.js
@@ -27,3 +27,46 @@ while (numArray[opCodeCount] !== 99) {
 };
 
 console.log(numArray[0]);
+
+//part2
+const getNewNumArray = (noun, verb) => {
+  let newArray = input.map(value => {
+    return Number(value);
+  });
+
+  newArray[1] = noun;
+  newArray[2] = verb;
+
+  return newArray;
+};
+
+const getOutput = numArray => {
+  let opCodeCount = 0;
+  while (numArray[opCodeCount] !== 99) {
+    let opCode = numArray[opCodeCount];
+
+    switch (opCode) {
+      case 1:
+        numArray[numArray[opCodeCount + 3]] = numArray[numArray[opCodeCount + 1]] + numArray[numArray[opCodeCount + 2]];
+        break;
+
+      case 2:
+        numArray[numArray[opCodeCount + 3]] = numArray[numArray[opCodeCount + 1]] * numArray[numArray[opCodeCount + 2]];
+        break;
+    }
+
+    opCodeCount += 4;
+  };
+
+  return numArray[0];
+};
+
+let stop = false;
+for (let noun = 0; noun < 100 && !stop; noun++) {
+  for (let verb = 0; verb < 100 && !stop; verb++) {
+    if (getOutput(getNewNumArray(noun, verb)) === 19690720) {
+      console.log(100 * noun + verb);
+      stop = true;
+    }
+  }
+}
