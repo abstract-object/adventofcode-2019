b6a377be6873027a778e484ab1f48ae25da46394
Johnathan X. Liu <jxliu95@gmail.com>
2019-12-12 04:20:13 -0500

    Complete day 5 part 2

--- day5-part2.js
+++ day5-part2.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const inputArray = fs.readFileSync("./day5-input.txt").toString().split(",");
 
-const inputValue = 1;
+const inputValue = 5;
 let outputValue = null;
 
 let opCodeCount = 0;
@@ -33,6 +33,36 @@ while (inputArray[opCodeCount].slice(-2) !== "99" && inputArray[opCodeCount].sli
     case 4:
       outputValue = Number(parameter1 ? inputArray[opCodeCount + 1] : inputArray[Number(inputArray[opCodeCount + 1])]);
       opCodeCount += 2;
+      break;
+    case 5:
+      if ((parameter1 ? Number(inputArray[opCodeCount + 1]) : Number(inputArray[Number(inputArray[opCodeCount + 1])])) !== 0) {
+        opCodeCount = Number(parameter2 ? Number(inputArray[opCodeCount + 2]) : Number(inputArray[Number(inputArray[opCodeCount + 2])]));
+      } else {
+        opCodeCount += 3;
+      }
+      break;
+    case 6:
+      if ((parameter1 ? Number(inputArray[opCodeCount + 1]) : Number(inputArray[Number(inputArray[opCodeCount + 1])])) === 0) {
+        opCodeCount = Number(parameter2 ? Number(inputArray[opCodeCount + 2]) : Number(inputArray[Number(inputArray[opCodeCount + 2])]));
+      } else {
+        opCodeCount += 3;
+      }
+      break;
+    case 7:
+      if ((parameter1 ? Number(inputArray[opCodeCount + 1]) : Number(inputArray[Number(inputArray[opCodeCount + 1])])) < (parameter2 ? Number(inputArray[opCodeCount + 2]) : Number(inputArray[Number(inputArray[opCodeCount + 2])]))) {
+        inputArray[Number(inputArray[opCodeCount + 3])] = "1";
+      } else {
+        inputArray[Number(inputArray[opCodeCount + 3])] = "0";
+      }
+      opCodeCount += 4;
+      break;
+    case 8:
+      if ((parameter1 ? Number(inputArray[opCodeCount + 1]) : Number(inputArray[Number(inputArray[opCodeCount + 1])])) === (parameter2 ? Number(inputArray[opCodeCount + 2]) : Number(inputArray[Number(inputArray[opCodeCount + 2])]))) {
+        inputArray[Number(inputArray[opCodeCount + 3])] = "1";
+      } else {
+        inputArray[Number(inputArray[opCodeCount + 3])] = "0";
+      }
+      opCodeCount += 4;
       break;
   }
 
