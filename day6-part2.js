33ad46e7ea6af7278087a93892f71edd20a66f30
Johnathan X. Liu <jxliu95@gmail.com>
2019-12-19 01:48:56 -0500

    Working solution, questionable code

--- day6-part2.js
+++ day6-part2.js
@@ -3,32 +3,45 @@ const fs = require("fs");
 const inputArray = fs.readFileSync("./day6-input.txt").toString().split("\n");
 newArray = inputArray.map(input => input.split(")"));
 
-const AOrbitsBMap = {};
-const BOrbitsAMap = {};
+const up = {};
+const down = {};
 
 newArray.forEach(orbit => {
-  AOrbitsBMap[orbit[1]] = orbit[0];
-  BOrbitsAMap[orbit[0]] = orbit[1];
+  if (up[orbit[1]]) {
+    up[orbit[1]].push(orbit[0]);
+  } else {
+    up[orbit[1]] = [orbit[0]];
+  }
+
+  if (down[orbit[0]]) {
+    down[orbit[0]].push(orbit[1]);
+  } else {
+    down[orbit[0]] = [orbit[1]];
+  }
 });
 
-const findSAN = (currentObj, prev) => {
-  let upCounter = 0;
-  let downCounter = 0;
-  let totalSteps = 0;
-
-  if (BOrbitsAMap[currentObj] === "SAN") {
-    return totalSteps;
-  } else if (downCounter <= upCounter && BOrbitsAMap[currentObj] !== prev) {
-    downCounter++;
-    totalSteps += 1;
-    return (Number.isNaN(findSAN(BOrbitsAMap[currentObj], currentObj)) ? 0 : findSAN(BOrbitsAMap[currentObj], currentObj)) + totalSteps;
-  } else if (upCounter <= downCounter && AOrbitsBMap[currentObj] !== prev) {
-    upCounter++;
-    totalSteps += 1;
-    return (Number.isNaN(findSAN(AOrbitsBMap[currentObj], currentObj)) ? 0 : findSAN(AOrbitsBMap[currentObj], currentObj)) + totalSteps;
+const findSAN = (currentObj, prev, total) => {
+  let newOrbits = [];
+  prev.push(currentObj);
+  if (currentObj === "SAN") {
+    console.log(total - 1);
+    return total;
+  } else if (down[currentObj] && !(down[currentObj].every(obj => prev.includes(obj)))) {
+    newOrbits = newOrbits.concat(down[currentObj].map(result => {
+      if (!prev.includes(result)) {
+        return findSAN(result, prev, total + 1);
+      }
+    }));
+  } else if (up[currentObj] && !(up[currentObj].every(obj => prev.includes(obj)))) {
+    newOrbits = newOrbits.concat(up[currentObj].map(result => {
+      if (!prev.includes(result)) {
+        return findSAN(result, prev, total + 1);
+      }
+    }));
   } else {
     return null;
   }
+  return findSAN(currentObj, prev, total);
 };
 
-console.log(findSAN(AOrbitsBMap["YOU"], "YOU"));
+findSAN(up["YOU"][0], ["YOU"], 0);
